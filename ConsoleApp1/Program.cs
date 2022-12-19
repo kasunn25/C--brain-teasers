34ae5de6f3664125b28ef3650e0e23ee329585a8
Kasun Wanniarachchi <kasun.wanniarachchi@nationstrust.com>
2022-12-19 09:49:56 +0530

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { };
+            Console.WriteLine(isOnionArray(arr) + "\n");
+        }
+
+        private static int isOnionArray(int[] a)
+        {
+            if (a.Length == 0) return 1;
+            if (a.Length == 1 && a[0] < 10) return 1;
+
+            for (int j = 0, k = a.Length - 1; j <= k; j++, k--)
+            {
+                if (a[j] + a[k] > 10)
+                    return 0;
+
+            }
+
+            return 1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# C--brain-teasers
+# C# brain-teasers
+
+OnionArray, 123Array, 235Array, isMartian
